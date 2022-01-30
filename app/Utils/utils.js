d7718b58ee8329fbdc8d9cfbcff90943f03a1db4
Tehleel <tehleelmir64@gmail.com>
2022-01-30 14:49:25 +0530

    networking related stuff all done, data is coming from the api based on the user input i.e. username

---
+++ app/Utils/utils.js
@@ -0,0 +1,96 @@
+import Constants                                              from '../Constants/Constants';
+import {UsernameSuccessResponse, UsernameFailureResponse,}    from '../Models/UsernameResponseModel';
+import {TweetsTimeSuccessResponse, TweetsTimeFailureResponse} from '../Models/TweetsTimeLineModel'
+import {ToastAndroid, Platform, AlertIOS}                     from 'react-native';
+
+
+export async function getIdByUsername(username, onCompleteListenerFun) {
+  try {
+    let response = await fetch(
+      `https://api.twitter.com/2/users/by/username/${username}`,
+      {
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${Constants.BearerToken}`,
+        },
+      },
+    );
+
+    let responseJson = await response.json();
+
+    if (responseJson.data != undefined || responseJson.data != null) {
+      getUserTweetTimelineById(
+        new UsernameSuccessResponse(responseJson.data),
+        onCompleteListenerFun,
+      );
+    } else {
+      let errorDetail = new UsernameFailureResponse(responseJson.errors);
+      if (Platform.OS === 'android') {
+        ToastAndroid.show(errorDetail.getErrorDetail(), ToastAndroid.SHORT);
+      } else {
+        AlertIOS.alert(errorDetail.getErrorDetail());
+      }
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function getUserTweetTimelineById(data, onCompleteListenerFun) {
+  try {
+    let response = await fetch(
+      `https://api.twitter.com/2/users/${data.getId()}/tweets?exclude=replies,retweets&tweet.fields=attachments&expansions=attachments.media_keys&media.fields=url&max_results=20`,
+      {
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${Constants.BearerToken}`,
+        },
+      },
+    );
+
+    let responseJson = await response.json();
+
+    if (responseJson.data != null || responseJson.data != undefined) {
+      let list = responseJson.data;
+      if (responseJson.includes != undefined)
+        onCompleteListenerFun(list, responseJson.includes, data.getId());
+      else onCompleteListenerFun(list, undefined, id);
+    } else {
+      let errorDetail = new TweetsTimeFailureResponse(responseJson.errors)
+      if(Platform.OS === 'android') {
+        ToastAndroid.show(errorDetail.getErrorDetail(), ToastAndroid.SHORT)
+      } else {
+        AlertIOS.alert(errorDetail.getErrorDetail()) 
+      }
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function loadMoreTweets(lastTweetId, userId, callBackFunction) {
+  try {
+    let response = await fetch(
+      `https://api.twitter.com/2/users/${userId}/tweets?exclude=replies,retweets&tweet.fields=attachments&expansions=attachments.media_keys&media.fields=url&max_results=20&until_id=${lastTweetId}`,
+      {
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${Constants.BearerToken}`,
+        },
+      },
+    );
+
+    let responseJson = await response.json();
+    
+    if (responseJson.data != null || responseJson.data != undefined) {
+      let list = responseJson.data;
+      if (responseJson.includes != undefined)
+        callBackFunction(list, responseJson.includes);
+      else callBackFunction(list, undefined);
+    } else {
+      console.log('No tweets from this user any more');
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,4 +1,7 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {getIdByUsername, loadMoreTweets} from '../Utils/utils';
+import {FlatListItem} from '../components/FlatListItem';
+import Constants from '../Constants/Constants';
 import {
   Text,
   Button,
@@ -6,10 +9,76 @@ import {
   View,
   TextInput,
   FlatList,
+  SafeAreaView,
 } from 'react-native';
 
-export default () => {
-  return <Text>Hello from Home screen</Text>;
-};
 
-const styles = StyleSheet.create({});
+export default function HomeScreen() {
+  let [usernameText, updateUserNameText]  = useState('');
+  let [listData, updateListData]          = useState([]);
+  let [media, updateMediaData]            = useState(undefined);
+  let [mUserId, updateUserId]             = useState(undefined);
+  let [appOpen, updateAppOpenFlag]        = useState(true);
+
+  if (appOpen) {
+    getIdByUsername(Constants.DefaultUsername, onCompleteListenerFun);
+    updateAppOpenFlag(false);
+  }
+
+  function onCompleteListenerFun(list, includes, userId) {
+    updateUserId(userId);
+    if (includes != undefined) updateMediaData(includes.media);
+    updateListData(list);
+  }
+
+  function loadMoreTweetsM() {
+    let lastTweetId = listData[listData.length - 1].id;
+
+    loadMoreTweets(lastTweetId, mUserId, (list, includes) => {
+      if (list.length > 0 && list != undefined)
+        updateListData(oldList => {
+          return [...oldList, ...list];
+        });
+      if (includes != undefined)
+        updateMediaData(oldData => {
+          return [...oldData, ...includes.media];
+        });
+    });
+  }
+
+  const renderItem = item => {
+    return <FlatListItem item={item.item} media={media} />;
+  };
+
+  return (
+    <View style={styles.root}>
+      <TextInput
+        value={usernameText}
+        placeholder="Enter User name here without @"
+        onChangeText={text => {
+          updateUserNameText(text);
+        }}
+      />
+
+      <Button
+        title="Look for this users tweets"
+        onPress={() => {
+          if (usernameText != '')
+            getIdByUsername(usernameText, onCompleteListenerFun);
+        }}
+      />
+
+      <FlatList
+        data={listData}
+        renderItem={renderItem}
+        onEndReached={loadMoreTweetsM}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  root: {
+    marginBottom: 100,
+  },
+});
